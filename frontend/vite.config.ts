0326c1be7f32e946cc72ce3e9066f3441e1e4e48
MrSmith <mamhigtt@gmail.com>
2026-06-01 22:54:28 +0700

    Add proxy configuration for '/files' endpoint in Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
         changeOrigin: true,
         ws: true,
       },
+      '/files': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
     },
   },
 });
